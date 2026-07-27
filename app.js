7cc45d2ab36633f007de36dc398d7aa5ae0b9881
canada2life-coder <canada2life@gmail.com>
2026-07-26 22:41:02 -0500

    Add files via upload
    First version

---
+++ app.js
@@ -0,0 +1,27 @@
+console.log(
+"SunWah Inventory Started"
+);
+
+
+
+let today =
+new Date()
+.toLocaleDateString();
+
+
+
+document.getElementById(
+"stockIn"
+).innerHTML = 0;
+
+
+
+document.getElementById(
+"stockOut"
+).innerHTML = 0;
+
+
+
+document.getElementById(
+"alerts"
+).innerHTML = 0;
